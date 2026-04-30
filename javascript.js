82679d2a0743ed33916de71ec1a042b2fdad5728
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-04-30 20:48:43 -0300

    Set up a hover effect when mouse passes over
    when the mouse passes over the little squares it changes the color

--- javascript.js
+++ javascript.js
@@ -2,10 +2,15 @@ const div = document.createElement("div");
 div.className = "div-square";
 const container = document.querySelector(".div-container");
 
-const a = 50;
+const a = 16;
 
 for (let i = 0; i < (a * a); i++){
     let rows = document.createElement("div");
     container.appendChild(rows);
     rows.setAttribute("style", `flex-grow: 0; box-sizing: border-box; flex-basis: calc(100% / ${a}); border: 1px solid black; aspect-ratio: 1 / 1;`);
+
+    rows.addEventListener("mouseover", () => {
+        rows.style.backgroundColor = "pink";
+    })
 }
+
