db174c0b22143c6087463cd451cd84b49e91548d
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-04-30 21:36:39 -0300

    Centralize the container and the button above
    centralized the container and the button with flexbox, also defined the
    minimum style of the button

--- javascript.js
+++ javascript.js
@@ -2,15 +2,11 @@ const div = document.createElement("div");
 div.className = "div-square";
 const container = document.querySelector(".div-container");
 
-const a = 16;
+const a = 25;
 
 for (let i = 0; i < (a * a); i++){
     let rows = document.createElement("div");
     container.appendChild(rows);
     rows.setAttribute("style", `flex-grow: 0; box-sizing: border-box; flex-basis: calc(100% / ${a}); border: 1px solid black; aspect-ratio: 1 / 1;`);
-
-    rows.addEventListener("mouseover", () => {
-        rows.style.backgroundColor = "pink";
-    })
 }
-
+ 
